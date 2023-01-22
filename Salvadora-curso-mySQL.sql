4d58c0430184b021ed8671516052913cf2edf4e2
ampinkus <ampinkus@gmail.com>
2023-01-22 13:24:40 -0300

    hasta capitulo 19

--- Salvadora-curso-mySQL.sql
+++ Salvadora-curso-mySQL.sql
@@ -3,11 +3,51 @@ SELECT * from cliente;
 select CI, nombre from cliente;
 SELECT * from salvadora.cliente WHERE  apellido = "cruz";
 SELECT * from producto where marca = "continental" and precio >= 3000;
+-- WHERE y LIKE
 SELECT * from salvadora.cliente WHERE  apellido like "c%"; -- cualquier apellido que comienza con "C"
 SELECT * from salvadora.cliente WHERE  apellido like "%z"; -- cualquier apellido que termina en "z"
 SELECT * from salvadora.cliente WHERE  apellido like "c"; -- cualquier apellido que comienza con "C"
 SELECT * from salvadora.proveedor WHERE ID_PROVEEDOR like "%0%"; -- cualquier proveedor que tenga un "0" en su ID
 SELECT * from salvadora.cliente WHERE CI like "18%"; -- cualquier cliente que su CI comience con 18
+SELECT * from salvadora.cliente WHERE CI not like "18%"; -- cualquier cliente que su CI no comience con 18
+SELECT * FROM salvadora.producto WHERE cod_producto like "10_"; -- que el código comience con 10 pero tenga solo 3 caracteres
+-- BETWEEN
+select nombre, marca, color, precio from producto WHERE precio BETWEEN 1500 and 3000; -- mostrar los registros donde precio esta entre 1500 y 3000
+select nombre, marca, color, precio from producto WHERE precio not BETWEEN 1500 and 3000; -- mostrar los registros donde precio no esta entre 1500 y 3000
+-- IN
+select * from producto WHERE  precio IN (1800, 2300, 3000); -- selecciono los productos que tienen esos tres precios 
+select nombre, marca, color from producto WHERE color  IN ("rojo", "blanco","negro");  -- selecciono los productos donde el color sea...
+-- sum() count() min() max()
+select sum(precio) from producto; -- summamos el precio de todos los artículos
+select count(id_proveedor) from proveedor;  -- contamos la cantidad de id_proveedor ( llave primaria) que hay en proveedor
+select min(precio) from producto; -- procuto con el valor minimo
+select max(precio) from producto; -- producto con el valor máximo
+-- GROUP BY
+select marca, sum(precio) from producto GROUP BY(marca); -- agrupo por marca la suma de los precios de cada marca
+select marca, count(*) from producto GROUP BY(marca); -- cuantos productos hay por cada marca
+select marca, count(*) from producto GROUP BY(marca) ORDER BY marca; -- cuantos productos hay por cada marca ordenados por marca
+select marca, count(*) from producto GROUP BY(marca) ORDER BY count(*); -- cuantos productos hay por cada marca ordenados por cantidad de productos de forma ascendente
+select marca, count(*) from producto WHERE color = "negro" GROUP BY(marca); -- cuantos productos hay de color negro por cada marca
+select marca, min(precio) FROM producto GROUP BY(marca); -- cual es el precio minimo de cada marca agrupado por marca 
+-- HAVING
+SELECT marca, sum(stock) from producto -- mostrar las marcas cuyo stock sea mas de 30 productos
+GROUP BY marca
+having sum(stock) > 30;
+SELECT modelo, sum(stock)   from producto -- mostrar los modelos cuyo stock sea mas de 30 productos
+GROUP BY modelo
+having sum(stock) > 30;
+SELECT color, count(*) from producto -- mostrar los productos por su color donde haya menos de 4 elementos del mismo color
+group by color
+having count(*) < 4;
+-- ORDER BY
+SELECT * from producto -- todos los productos de marca continental ordenados por nombre de forma descendente (por defecto es ascendente)
+WHERE marca = "continental"
+ORDER BY nombre DESC; 
+SELECT * from producto -- todos los productos de marca con primera letra b ordenados por nombre de forma ascendente
+WHERE marca LIKE "b%"
+ORDER BY marca ASC; 
+
+
 
 
 
